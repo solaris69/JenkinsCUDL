f9c478645c702c046d18da04953e275fdd597e92
solaris69 <vmanin@outlook.com>
2015-01-05 13:35:24 +0700

    Update to support build kettle.rpm

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    buildIndex: grunt.file.readJSON('build.json'),
+    //buildIndex: grunt.file.readJSON('build.json'),
     banner: '/* <%= pkg.title || pkg.name %> -v <%= pkg.version %>*/  \n ',
 
     distrib: {
@@ -40,43 +40,49 @@ module.exports = function(grunt) {
     ,easy_rpm: {
         options: {
             //   summary: 'Creating rpm packages!',
+          /*
             name: "<%=pkg.name %>"
             ,version: "<%= pkg.version %>.<%= buildIndex.build %>"
             ,release: "<%= meta.revision %>"
             //,release: "1234"
             ,buildArch: "x86_64"
 
-            /*
+
+           ,preInstallScript: [
+           'if [ ! -d "/compass/integration/app/<%=pkg.name %>" ]; then echo "/compass/integration/app/<%=pkg.name %> not exist. Create new one"; sudo mkdir -p /compass/integration/app/<%=pkg.name %>; fi'
+           ,'logger -s Backup previous version of <%=pkg.name %>'
+           , 'if [ -d "/compass/integration/app/<%=pkg.name %>.previous" ]; then echo "Remove previous version /compass/integration/app/<%=pkg.name %>.previous"; sudo rm -r /compass/integration/app/<%=pkg.name %>.previous; cp -pr /compass/integration/app/<%=pkg.name %> /compass/integration/app/<%=pkg.name %>.previous; fi'
+           , 'sudo rm -r /compass/integration/app/<%=pkg.name %>'
+           ]
+
+           ,postInstallScript: [
+           'logger -s link deployed version to release directory'
+           ,'sudo ln -sf /compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %> /compass/integration/app/<%=pkg.name %>'
+           ]
+            */
+
             // Below is for Kettle binary to rpm
-            name: "Kettle-data-integration"
-            ,version: " 5.1.0.0"
-            ,release: "752"
+            name: "<%=pkg.pentahoName %>"
+            ,version: "<%=pkg.pentahoVersion %>"
+            ,release: "<%=pkg.pentahoRelease %>"
             ,buildArch: "x86_64"
-*/
+
 
             ,preInstallScript: [
-                'if [ ! -d "/compass/integration/app/<%=pkg.name %>" ]; then echo "/compass/integration/app/<%=pkg.name %> not exist. Create new one"; sudo mkdir -p /compass/integration/app/<%=pkg.name %>; fi'
-                ,'logger -s Backup previous version of <%=pkg.name %>'
-                , 'if [ -d "/compass/integration/app/<%=pkg.name %>.previous" ]; then echo "Remove previous version /compass/integration/app/<%=pkg.name %>.previous"; sudo rm -r /compass/integration/app/<%=pkg.name %>.previous; cp -pr /compass/integration/app/<%=pkg.name %> /compass/integration/app/<%=pkg.name %>.previous; fi'
-                , 'sudo rm -r /compass/integration/app/<%=pkg.name %>'
-//                'logger -s Stopping forever-crab-api service',
-//                'sudo service forever-crab-api stop',
-//                'if [ -L "/compass/permission/web/crab-api" ]; then echo "/compass/permission/web/crab-api exist"; sudo rm /compass/permission/web/crab-api.previous; cp -pr /compass/permission/web/crab-api  /compass/permission/web/crab-api.previous; sudo rm  /compass/permission/web/crab-api; else echo "/compass/permission/web/crab-api not exist"; fi'
+                'logger -s Backup previous version of <%=pkg.pentahoName %>'
+                , 'if [ -d "/compass/integration/app/<%=pkg.pentahoName %>.previous" ]; then echo "Remove previous version /compass/integration/app/<%=pkg.pentahoName %>.previous"; sudo rm -r /compass/integration/app/<%=pkg.name %>.previous; cp -pr /compass/integration/app/<%=pkg.name %> /compass/integration/app/<%=pkg.name %>.previous; fi'
+                , 'if [ -d "/compass/integration/app/pentaho" ]; then sudo rm -r /compass/integration/app/pentaho; fi'
             ]
 
             ,postInstallScript: [
                 'logger -s link deployed version to release directory'
-//                ,'sudo ln -sf /compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %>-<%= meta.revision %>/distrib /compass/integration/app/<%=pkg.name %>'
-////                ,'sudo ln -sf /compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %>/distrib /compass/integration/app/<%=pkg.name %>'
-                ,'sudo ln -sf /compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %> /compass/integration/app/<%=pkg.name %>'
-//                'logger -s Starting forever-crab-api service',
-//                'sudo service forever-crab-api start'
+                ,'sudo ln -sf /compass/integration/app/<%=pkg.pentahoName %>-<%=pkg.pentahoVersion %>.<%=pkg.pentahoRelease %> /compass/integration/app/pentaho'
             ]
         },
         release: {
             files: [
-                { cwd: '<%= distrib.root%>/', src: "**", dest: "/compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %>"}
-//                { cwd: 'Kettle/', src: "**", dest: "/compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %>"}
+//                { cwd: '<%= distrib.root%>/', src: "**", dest: "/compass/integration/app/<%=pkg.name %>-<%= pkg.version %>.<%= buildIndex.build %>"}
+                { cwd: 'Kettle/', src: "**", dest: "/compass/integration/app/<%=pkg.pentahoName %>-<%=pkg.pentahoVersion %>.<%=pkg.pentahoRelease %>"}
             ]
         }
     }
@@ -97,5 +103,6 @@ module.exports = function(grunt) {
   grunt.registerTask("build-rpm", ['clean:distrib', 'copy:all', 'revision', 'easy_rpm']); // task for rpm via git repo
   grunt.registerTask("pentaho-rpm", ['easy_rpm']); // place Kettle binary to the folder name "Kettle" then update field name,version, and release in options.
     // Remove pre/post installation script because Kettle doesn't need it. Finally, update dest field in files to be Kettle target directory.
+    // DONT'T forget to comment build.json. this pentaho.rpm doesn't need it.
 //  grunt.registerTask("build-rpm", ['clean:distrib', 'copy:all', 'easy_rpm']); // offline test. No git repo
 };
